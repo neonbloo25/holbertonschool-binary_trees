ac61b86a2b0b156a9108af2ce46f0ce353d804ae
theluckdemon <theluckdemon@gmail.com>
2024-12-13 15:49:45 -0400

    to 3

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,7 +1,12 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_delete - no more tree
+ * @tree: THE tree
+ * Return: nuffin
+ */
 void binary_tree_delete(binary_tree_t *tree)
 {
-	if(tree == NULL)
+	if (tree == NULL)
 		return;
 
 	binary_tree_delete(tree->right);
