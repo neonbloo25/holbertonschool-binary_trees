09199fd0140a46c99fbb5b36088a9eb26023b925
theluckdemon <theluckdemon@gmail.com>
2024-12-14 11:58:57 -0400

    reformat

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -6,13 +6,7 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (node == NULL)
-		return (0);
-
-	if (node->parent == NULL)
-		return (0);
-	else if (node->parent != NULL)
+	if (node && node->left == NULL && node->right == NULL)
 		return (1);
-	else
-		return (0);
+	return (0);
 }
