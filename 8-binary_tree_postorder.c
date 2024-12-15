76139b4701717f5446911c6fd113ead1bb37d427
theluckdemon <theluckdemon@gmail.com>
2024-12-15 16:48:41 -0400

    to 8

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -11,6 +11,6 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 
 	/*here's where im guessing its best to have left first then right*/
 	binary_tree_postorder(tree->right, func);
-	binary_tree_postorder(tree->left, func);
 	func(tree->n);
+	binary_tree_postorder(tree->left, func);
 }
