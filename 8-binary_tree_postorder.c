078ab953e8e415bd0a4c644c6f235573a7b9ae33
theluckdemon <theluckdemon@gmail.com>
2024-12-15 16:39:41 -0400

    to 8

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,16 @@
+#include"binary_trees.h"
+/**
+ * binary_tree_postorder - toggles through nodes in postorder fashion
+ * @tree: pointer
+ * @func: preorder function
+ */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+
+	func(tree->n);
+	/*here's where im guessing its best to have left first then right*/
+	binary_tree_postorder(tree->right, func);
+	binary_tree_postorder(tree->left, func);
+}
